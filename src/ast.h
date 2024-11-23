90c14ccfb85e969d2e8e3c2afc010ffbbcc72184
RajDaBest <imagination12344321.com@gmail.com>
2024-11-24 05:29:21 +0530

    ast

--- src/ast.h
+++ src/ast.h
@@ -10,40 +10,53 @@ typedef struct statement_ statement_t;
 typedef struct return_statement_ return_statement_t;
 typedef struct expression_ expression_t;
 
-typedef enum {
-    CONSTANT_INT,   
+typedef enum
+{
+    CONSTANT_INT,
 } expression_type_t;
 
-struct expression_ {
-    expression_type_t expression_type;  
-    union {
+struct expression_
+{
+    expression_type_t expression_type;
+    union
+    {
         int constant_int;
-
     } expression;
 };
 
-struct return_statement_ {
+struct return_statement_
+{
     expression_t *expression;
 };
 
-typedef enum {
+typedef enum
+{
     RETURN,
 } statement_type_t;
 
-struct statement_ {
-    statement_type_t statement_type;  
-    union {
+struct statement_
+{
+    statement_type_t statement_type;
+    union
+    {
         return_statement_t return_statement;
     } statement;
 };
 
-struct function_definition_ {
-    identifier_t *name;  
-    statement_t *body;   
+struct function_definition_
+{
+    identifier_t *identifier;
+    statement_t *statement;
 };
 
-struct program_ {
+struct program_
+{
     function_definition_t *function_definition;
+};
+
+struct identifier_
+{
+    char name[MAX_TOKEN_LENGTH];
 };
 
 #endif /* EF013AD5_C306_4868_9D0B_F237FCCEDD09 */
