8b354c3f5ea48db9228d2cb0c9b726becca713dc
RajDaBest <imagination12344321.com@gmail.com>
2024-11-25 03:13:13 +0530

    added allocator

--- src/ast.h
+++ src/ast.h
@@ -11,7 +11,8 @@ typedef struct expression expression_t;
 typedef struct identifier identifier_t;
 typedef struct program program_t;
 
-typedef enum {
+typedef enum
+{
     NODE_PROGRAM,
     NODE_FUNCTION_DEF,
     NODE_STATEMENT,
@@ -19,65 +20,64 @@ typedef enum {
     NODE_IDENTIFIER
 } ast_node_type_t;
 
-typedef enum {
+typedef enum
+{
     EXPR_CONSTANT_INT
 } expression_type_t;
 
-typedef enum {
+typedef enum
+{
     STMT_RETURN
 } statement_type_t;
 
-struct ast_node {
+struct ast_node
+{
     ast_node_type_t type;
-    struct {
+    struct
+    {
         size_t line;
         size_t column;
     } location;
-    ast_node_t* parent;
+    ast_node_t *parent;
 };
 
-struct identifier {
+struct identifier
+{
     ast_node_t base;
-    char* name;  
+    char *name;
 };
 
-struct expression {
+struct expression
+{
     ast_node_t base;
     expression_type_t expr_type;
-    union {
+    union
+    {
         int constant_int;
     } value;
 };
 
-struct statement {
+struct statement
+{
     ast_node_t base;
     statement_type_t stmt_type;
-    union {
-        expression_t* return_expr;
+    union
+    {
+        expression_t *return_expr;
     } value;
 };
 
-struct function_def {
+struct function_def
+{
     ast_node_t base;
-    identifier_t* name;
-    statement_t* body;
+    identifier_t *name;
+    statement_t *body;
 };
 
-struct program {
+struct program
+{
     ast_node_t base;
-    function_def_t* function;
+    function_def_t *function;
 };
-
-program_t* create_program(function_def_t* function);
-function_def_t* create_function(identifier_t* name, statement_t* body);
-statement_t* create_return_statement(expression_t* expr);
-expression_t* create_constant_int(int value);
-identifier_t* create_identifier(const char* name);
-
-void free_ast(ast_node_t* node);
-
-bool ast_validate(const ast_node_t* node);
-void ast_set_location(ast_node_t* node, size_t line, size_t column);
-const char* ast_node_type_to_string(ast_node_type_t type);
 
 #endif /* AST_H */
--- src/parser.h
+++ src/parser.h
@@ -3,5 +3,22 @@
 
 #include "lexer.h"
 #include "ast.h"
+#include "allocator.h"
+
+typedef parser_ parser_t;
+
+struct parser_
+{
+    token_t *tokens;
+    size_t current_index;
+    size_t token_count;
+};
+
+parser_t *init_parser(token_t *tokens);
+program_t *parse(parser_t *parser);
+token_t *current_token(parser_t *parser);
+token_t *next_token(parser_t *parser);
+bool match_token(parser_t *parser, token_type_t expected);
+void error(const char *message, size_t line, size_t column);
 
 #endif /* FAF9272A_6BB8_4D5B_AE9C_2B710AB564BC */
