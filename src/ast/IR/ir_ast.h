68d1b9190ae98666941b42d3fa09a389a3696f8d
RajDaBest <imagination12344321.com@gmail.com>
2025-01-01 00:20:16 +0530

    new ir ast

--- src/ast/IR/ir_ast.h
+++ src/ast/IR/ir_ast.h
@@ -103,11 +103,44 @@ typedef struct ir_instruction_binary_e
     ir_value_t *destination;
 } ir_instruction_binary_t;
 
+typedef struct ir_instruction__copy_e
+{
+    ir_value_t *source;
+    ir_value_t *destination;
+} ir_instruction_copy_t;
+
+typedef struct ir_instruction_jump_e
+{
+    identifier_t *target;
+} ir_instruction_jump_t;
+
+typedef struct ir_instruction_jz_e
+{
+    ir_value_t *condition;
+    identifier_t *target;
+} ir_instruction_jz_t;
+
+typedef struct ir_instruction_jnz_e
+{
+    ir_value_t *condition;
+    identifier_t *target;
+} ir_instruction_jnz_t;
+
+typedef struct ir_instruction_label_e
+{
+    identifier_t *identifier;
+} ir_instruction_label_t;
+
 typedef enum
 {
     IR_INSTR_RETURN,
     IR_INSTR_UNARY,
     IR_INSTR_BINARY,
+    IR_INSTR_COPY,
+    IR_INSTR_JUMP,
+    IR_INSTR_JUMP_IF_ZERO,
+    IR_INSTR_JUMP_IF_NOT_ZERO,
+    IR_INSTR_LABEL,
 } ir_instruction_type_t;
 
 typedef struct
@@ -131,6 +164,11 @@ typedef struct ir_instruction_e
         ir_instruction_return_t return_instr;
         ir_instruction_unary_t unary_instr;
         ir_instruction_binary_t binary_instr;
+        ir_instruction_copy_t copy_instr;
+        ir_instruction_jump_t jmp_instr;
+        ir_instruction_jz_t jz_instr;
+        ir_instruction_jnz_t jnz_instr;
+        ir_instruction_label_t label_instr;
     } instruction;
 } ir_instruction_t;
 
