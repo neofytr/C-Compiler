444f1490fef0d7272a588d85365717647165383e
RajDaBest <imagination12344321.com@gmail.com>
2025-01-01 21:20:34 +0530

    new assembly ast

--- src/ast/assembly/assembly_ast.h
+++ src/ast/assembly/assembly_ast.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+// Forward declarations for all struct types
 typedef struct asm_program_ asm_program_t;
 typedef struct asm_function_ asm_function_t;
 typedef struct asm_identifier_ asm_identifier_t;
@@ -20,9 +21,15 @@ typedef struct asm_instruction_unary_ asm_instruction_unary_t;
 typedef struct asm_unary_operator_ asm_unary_operator_t;
 typedef struct asm_binary_operator_ asm_binary_operator_t;
 typedef struct asm_instruction_binary_ asm_instruction_binary_t;
+typedef struct asm_instruction_cmp_ asm_instruction_cmp_t;
+typedef struct asm_instruction_jmp_ asm_instruction_jmp_t;
+typedef struct asm_instruction_jmpcc_ asm_instruction_jmpcc_t;
+typedef struct asm_instruction_setcc_ asm_instruction_setcc_t;
+typedef struct asm_instruction_label_ asm_instruction_label_t;
+typedef struct asm_instruction_cdq_ asm_instruction_cdq_t;
 typedef struct asm_instruction_idiv_ asm_instruction_idiv_t;
-typedef struct asm_instruction_cqo_ asm_instruction_cqo_t;
 
+// AST node types
 typedef enum
 {
     ASM_NODE_PROGRAM,
@@ -38,6 +45,7 @@ struct asm_ast_node_
     asm_ast_node_t *parent;
 };
 
+// Operand types
 typedef enum
 {
     OPERAND_IMMEDIATE,
@@ -46,16 +54,32 @@ typedef enum
     OPERAND_STACK,
 } asm_operand_type_t;
 
+// Instruction types
 typedef enum
 {
     INSTRUCTION_MOV,
     INSTRUCTION_RET,
     INSTRUCTION_UNARY,
-    INSTRUCTION_ALLOCATE_STACK,
-    INSTRUCTION_CQO,
-    INSTRUCTION_IDIV,
     INSTRUCTION_BINARY,
+    INSTRUCTION_CMP,
+    INSTRUCTION_IDIV,
+    INSTRUCTION_CDQ,
+    INSTRUCTION_JMP,
+    INSTRUCTION_JMPCC,
+    INSTRUCTION_SETCC,
+    INSTRUCTION_LABEL,
+    INSTRUCTION_ALLOCATE_STACK,
 } asm_instruction_type_t;
+
+typedef enum
+{
+    COND_E,  // Equal
+    COND_NE, // Not Equal
+    COND_SIGN_G,  // Signed Greater
+    COND_SIGN_GE, // Signed Greater or Equal
+    COND_SIGN_L,  // Signed Less
+    COND_SIGN_LE, // Signed Less or Equal
+} asm_condition_code_t;
 
 struct asm_immediate_
 {
@@ -64,11 +88,10 @@ struct asm_immediate_
 
 typedef enum
 {
-    ASM_REG_RAX,
+    ASM_REG_AX,
+    ASM_REG_DX,
     ASM_REG_R10,
-    ASM_REG_RDX,
     ASM_REG_R11,
-    ASM_REG_RCX,
 } asm_reg_no_t;
 
 struct asm_register_
@@ -84,9 +107,7 @@ struct asm_pseudo_
 struct asm_stack_
 {
     int offset;
-}; // ultimately, we need to replace every pseudoreg with a location on the stack;
-// we represent those with the stack operand, which indicates the stack address at the
-// given offset from RBP; we'd represent the operand (RBP - 4) with AST node Stack(-4)
+};
 
 struct asm_identifier_
 {
@@ -101,9 +122,8 @@ struct asm_operand_
     union
     {
         asm_immediate_t immediate;
-        asm_register_t reg;  // represents a hardware register
-        asm_pseudo_t pseudo; // lets us use an arbitrary identifier as a pseudoregister; we use this to
-        // refer to the temporary variables produced during IR generation
+        asm_register_t reg;
+        asm_pseudo_t pseudo;
         asm_stack_t stack;
     } operand;
 };
@@ -114,10 +134,25 @@ struct asm_instruction_mov_
     asm_operand_t *dst;
 };
 
+struct asm_instruction_ret_
+{
+};
+
+typedef enum
+{
+    UNARY_NEG,
+    UNARY_NOT,
+} asm_unary_operator_enum_t;
+
+struct asm_unary_operator_
+{
+    asm_unary_operator_enum_t op;
+};
+
 struct asm_instruction_unary_
 {
     asm_unary_operator_t *unary_operator;
-    asm_operand_t *operand; // is both the source and the destination operand
+    asm_operand_t *operand;
 };
 
 typedef enum
@@ -125,11 +160,6 @@ typedef enum
     ASM_BINARY_ADD,
     ASM_BINARY_SUB,
     ASM_BINARY_MULT,
-    ASM_BINARY_BITWISE_AND,
-    ASM_BINARY_BITWISE_OR,
-    ASM_BINARY_BITWISE_XOR,
-    ASM_BINARY_BITWISE_SHIFT_LEFT,
-    ASM_BINARY_BITWISE_SHIFT_RIGHT,
 } asm_binary_operator_type_t;
 
 struct asm_binary_operator_
@@ -144,33 +174,46 @@ struct asm_instruction_binary_
     asm_operand_t *second_operand;
 };
 
+struct asm_instruction_cmp_
+{
+    asm_operand_t *first_operand;
+    asm_operand_t *second_operand;
+};
+
+struct asm_instruction_jmp_
+{
+    asm_identifier_t *target;
+};
+
+struct asm_instruction_jmpcc_
+{
+    asm_condition_code_t condition;
+    asm_identifier_t *target;
+};
+
+struct asm_instruction_setcc_
+{
+    asm_condition_code_t condition;
+    asm_operand_t *dst;
+};
+
+struct asm_instruction_label_
+{
+    asm_identifier_t *label;
+};
+
+struct asm_instruction_cdq_
+{
+};
+
 struct asm_instruction_idiv_
 {
     asm_operand_t *operand;
 };
 
-struct asm_instruction_cqo_
-{
-};
-
 struct asm_instruction_alloc_stack_
 {
-    int alloc_size; // is the instruction sub rsp, alloc_size
-};
-
-typedef enum
-{
-    UNARY_NEG,
-    UNARY_NOT,
-} asm_unary_operator_enum_t;
-
-struct asm_unary_operator_
-{
-    asm_unary_operator_enum_t op;
-};
-
-struct asm_instruction_ret_
-{
+    int alloc_size;
 };
 
 struct asm_instruction_
@@ -182,10 +225,15 @@ struct asm_instruction_
         asm_instruction_mov_t mov;
         asm_instruction_ret_t ret;
         asm_instruction_unary_t unary;
-        asm_instruction_alloc_stack_t alloc_stack;
         asm_instruction_binary_t binary;
-        asm_instruction_cqo_t cqo;
+        asm_instruction_cmp_t cmp;
         asm_instruction_idiv_t idiv;
+        asm_instruction_cdq_t cdq;
+        asm_instruction_jmp_t jmp;
+        asm_instruction_jmpcc_t jmpcc;
+        asm_instruction_setcc_t setcc;
+        asm_instruction_label_t label;
+        asm_instruction_alloc_stack_t alloc_stack;
     } instr;
 };
 
@@ -193,7 +241,7 @@ struct asm_function_
 {
     asm_ast_node_t base;
     asm_identifier_t *name;
-    asm_instruction_t **instructions; // Dynamic array of instruction pointers
+    asm_instruction_t **instructions;
     size_t instruction_count;
 };
 
