ffeb9e8429cff7d39c9ec494e2bcf2ea83ab3153
RajDaBest <imagination12344321.com@gmail.com>
2025-01-01 22:43:33 +0530

    everything working uptil now

--- test/output.asm
+++ test/output.asm
@@ -3,12 +3,14 @@ global main
 main:
     push rbp
     mov rbp, rsp
-    sub rsp, 4
-    mov qword [rbp-4], 12
-    mov r11, [rbp-4]
-    imul r11, 7
-    mov qword [rbp-4], r11
-    mov rax, [rbp-4]
+    sub rsp, 8
+    mov qword [rbp-4], 7
+    neg qword [rbp-4]
+    mov qword [rbp-8], 12
+    mov r11, [rbp-8]
+    imul r11, [rbp-4]
+    mov qword [rbp-8], r11
+    mov rax, [rbp-8]
     mov rsp, rbp
     pop rbp
     ret
